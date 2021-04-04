ca721b760ce34df9a4d3e081a737fb1f6eefed4d
Pirgo <pierogmateusz@wp.pl>
2021-04-04 12:27:54 +0200

    added test

--- classes/layouts.py
+++ classes/layouts.py
@@ -39,10 +39,8 @@ def evel_hand(hand):  #7-cards
     #looking for Quads
     quads_counter = 1
     res_cards = [sorted_cards[0]]
-    quads_value = sorted_cards[0].get_rank()
     for i in range(1, len(sorted_cards)):
         if sorted_cards[i].get_rank() == sorted_cards[i-1].get_rank():
-            quads_value = sorted_cards[i].get_rank()
             quads_counter+=1
             res_cards.append(sorted_cards[i])
             if(quads_counter == 4):
@@ -54,7 +52,7 @@ def evel_hand(hand):  #7-cards
         else:
             quads_counter = 1
             res_cards = [sorted_cards[i]]
-            quads_value = sorted_cards[i].get_rank()
+
 
 
     pairs = []
@@ -142,215 +140,75 @@ def evel_hand(hand):  #7-cards
     return NO_PAIR, [x for x in sorted_cards[0:5]]
 
 
-def solve_draw_straight_flush(players, same_hand_weight_list, players_hands):
-    maks_card = -1
-    for i in same_hand_weight_list:
-        if players_hands[i][0].get_rank() > maks_card:
-            maks_card = players_hands[i][0].get_rank()
-
-    return [players[i] for i in same_hand_weight_list if players_hands[i][0].get_rank() == maks_card]
-
-def solve_draw_quads(players, same_hand_weight_list, players_hands):
-    maks_card = -1 #ostatnia karta sie liczy
-    for i in same_hand_weight_list:
-        if players_hands[i][4].get_rank() > maks_card:
-            maks_card = players_hands[i][4].get_rank()
-
-
-    return [players[i] for i in same_hand_weight_list if players_hands[i][4].get_rank() == maks_card]
-
-def solve_draw_full_house(players, same_hand_weight_list, players_hands):
-    maks_three_card = -1
-    maks_pair_card = -1
-    for i in same_hand_weight_list:
-        if players_hands[i][0].get_rank() > maks_three_card:
-            maks_three_card = players_hands[i][0].get_rank()
-            maks_pair_card = players_hands[i][3].get_rank()
-        elif players_hands[i][0].get_rank() == maks_three_card:
-            if players_hands[i][3].get_rank() > maks_pair_card:
-                maks_pair_card = players_hands[i][3].get_rank()
-
-    return [players[i] for i in same_hand_weight_list if players_hands[i][0].get_rank() == maks_three_card and players_hands[i][3].get_rank() == maks_pair_card]
-
-def solve_draw_flush(players, same_hand_weight_list, players_hands):
-    maks_card = -1  # ostatnia karta sie liczy
-    for i in same_hand_weight_list:
-        if players_hands[i][0].get_rank() > maks_card:
-            maks_card = players_hands[i][0].get_rank()
-
-    return [players[i] for i in same_hand_weight_list if players_hands[i][0].get_rank() == maks_card]
-
-def solve_draw_straight(players, same_hand_weight_list, players_hands):
-    maks_card = -1  #ostatnia karta sie liczy
-    for i in same_hand_weight_list:
-        if players_hands[i][0].get_rank() > maks_card:
-            maks_card = players_hands[i][0].get_rank()
-
-    return [players[i] for i in same_hand_weight_list if players_hands[i][0].get_rank() == maks_card]
-
-def solve_draw_Trips(players, same_hand_weight_list, players_hands):
-    maks_tripe_card = -1
-    maks_first_greater_card = -1
-    maks_second_greater_card = -1
-    for i in same_hand_weight_list:
-        if players_hands[i][0].get_rank() > maks_tripe_card:
-            maks_tripe_card = players_hands[i][0].get_rank()
-            maks_first_greater_card = players_hands[i][3].get_rank()
-            maks_second_greater_card = players_hands[i][4].get_rank()
-        elif players_hands[i][0].get_rank() == maks_tripe_card:
-            if players_hands[i][3].get_rank() > maks_first_greater_card:
-                maks_first_greater_card = players_hands[i][3].get_rank()
-                maks_second_greater_card = players_hands[i][4].get_rank()
-            elif players_hands[i][3].get_rank() == maks_first_greater_card:
-                if players_hands[i][4].get_rank() > maks_second_greater_card:
-                    maks_second_greater_card = players_hands[i][4].get_rank()
-    return [players[i] for i in same_hand_weight_list if players_hands[i][0].get_rank() == maks_tripe_card and
-            players_hands[i][3].get_rank() == maks_first_greater_card and
-            players_hands[i][4].get_rank() == maks_second_greater_card]
-
-def solve_draw_Two_Pairs(players, same_hand_weight_list, players_hands):
-    maks_first_pair_card = -1
-    maks_second_pair_card = -1
-    maks_kicker_card = -1
-    for i in same_hand_weight_list:
-        if players_hands[i][0].get_rank() > maks_first_pair_card:
-            maks_first_pair_card = players_hands[i][0].get_rank()
-            maks_second_pair_card = players_hands[i][2].get_rank()
-            maks_kicker_card = players_hands[i][4].get_rank()
-        elif players_hands[i][0].get_rank() == maks_first_pair_card:
-            if players_hands[i][2].get_rank() > maks_second_pair_card:
-                maks_second_pair_card = players_hands[i][2].get_rank()
-                maks_kicker_card = players_hands[i][4].get_rank()
-            elif players_hands[i][2].get_rank() == maks_second_pair_card:
-                if players_hands[i][4].get_rank() > maks_kicker_card:
-                    maks_kicker_card = players_hands[i][4].get_rank()
-
-    return [players[i] for i in same_hand_weight_list if players_hands[i][0].get_rank() == maks_first_pair_card and
-            players_hands[i][3].get_rank() == maks_second_pair_card and
-            players_hands[i][4].get_rank() == maks_kicker_card]
-
-def solve_draw_One_Pair(players, same_hand_weight_list, players_hands):
-    maks_pair_card = -1
-    maks_first_kicker = -1
-    maks_second_kicker = -1
-    maks_third_kicker = -1
-    for i in same_hand_weight_list:
-        if players_hands[i][0].get_rank() > maks_pair_card:
-            maks_pair_card = players_hands[i][0].get_rank()
-            maks_first_kicker = players_hands[i][2].get_rank()
-            maks_second_kicker = players_hands[i][3].get_rank()
-            maks_third_kicker = players_hands[i][4].get_rank()
-        elif players_hands[i][0].get_rank() == maks_pair_card:
-            if players_hands[i][2].get_rank() > maks_first_kicker:
-                maks_first_kicker = players_hands[i][2].get_rank()
-                maks_second_kicker = players_hands[i][3].get_rank()
-                maks_third_kicker = players_hands[i][4].get_rank()
-            elif players_hands[i][2].get_rank() == maks_first_kicker:
-                if players_hands[i][3].get_rank() > maks_second_kicker:
-                    maks_second_kicker = players_hands[i][3].get_rank()
-                    maks_third_kicker = players_hands[i][4].get_rank()
-                elif players_hands[i][3].get_rank() == maks_second_kicker:
-                    if players_hands[i][4].get_rank() > maks_third_kicker:
-                        maks_third_kicker = players_hands[i][4].get_rank()
-
-    return [players[i] for i in same_hand_weight_list if players_hands[i][0].get_rank() == maks_pair_card and
-            players_hands[i][2].get_rank() == maks_first_kicker and
-            players_hands[i][3].get_rank() == maks_second_kicker and
-            players_hands[i][4].get_rank() == maks_third_kicker]
-
-def get_biggest(same_hand_weight_list, players_hands,n):
+def get_biggest_rank(players_hands, n):
     res = -1
-    for i in same_hand_weight_list:
-        if res < players_hands[i][n].get_rank():
-            res = players_hands[i][n].get_rank()
+    for hand in players_hands:
+        if res < hand[n].get_rank():
+            res = hand[n].get_rank()
     return res
 
-def solve_draw_biggest_card(players,same_hand_weight_list, players_hands):
-    for _ in range(0,5):
-        biggest_card = get_biggest(same_hand_weight_list, players_hands, _)
-        for i in same_hand_weight_list:
-            if players_hands[i][_].get_rank() != biggest_card:
-                same_hand_weight_list.remove(i)
-    return [players[i] for i in same_hand_weight_list]
+
+def solve_draw(players, players_hands):
+    winning_players = players.copy()
+    winning_hands = players_hands.copy()
+    for i in range(5):
+        biggest_rank = get_biggest_rank(players_hands, i)
+        for player, hand in list(zip(winning_players, winning_hands)):
+            if hand[i].get_rank() != biggest_rank:
+                players_hands.remove(hand)
+                players.remove(player)
+        winning_hands = players_hands.copy()
+        winning_players = players.copy()
+
+    return winning_players
 
 def point_the_winner(players, table_cards): #lista graczy(do ich kart odwolujemy sie poprzez player.cards oraz player.get_cards,
     #oraz table_cards - to już jest gotowa lista 3 kart ktore sa na stole czyli działamy na 2 listach
-
+    n = len(players)
     players_hand_value = []
     players_hands = []
-    maks_value = -1
 
-    #print(table_cards + players[j].cards) sprawdzic czy dziala
-    for j in range(len(players)):
+    for j in range(n):
         tmp = evel_hand(table_cards + players[j].cards)
         players_hands.append(tmp[1])
         players_hand_value.append(tmp[0])
 
     maks_value = max(players_hand_value)
 
+
     if players_hand_value.count(maks_value) == 1:
         return [players[players_hand_value.index(maks_value)]]
     else:
         same_hand_weight_list = []
         #find list of idexes of the list "players" that have the same hand weight
-        for _ in range(len(players_hand_value)):
-            if players_hand_value[_] == maks_value:
-                same_hand_weight_list.append(_)
+        for i in range(len(players_hand_value)):
+            if players_hand_value[i] == maks_value:
+                same_hand_weight_list.append(i)
+
+        players_same_weight = []
+        players_hands_same_weight = []
+        for i in same_hand_weight_list:
+            players_same_weight.append(players[i])
+            players_hands_same_weight.append(players_hands[i])
 
 
-        #STRAIGHT_FLUSH
-        if maks_value == 8:
-            return solve_draw_straight_flush(players, same_hand_weight_list, players_hands)
-
-        #quads:
-        if maks_value == 7:
-            return solve_draw_quads(players, same_hand_weight_list, players_hands)
-
-        #full_house:
-        if maks_value == 6:
-            return solve_draw_full_house(players, same_hand_weight_list, players_hands)
-
-        #Flush
-        if maks_value == 5:
-            return solve_draw_flush(players, same_hand_weight_list, players_hands)
-
-        #streight:
-        if maks_value == 4:
-            return solve_draw_straight(players, same_hand_weight_list, players_hands)
-
-        #Trips
-        if maks_value == 3:
-            return solve_draw_Trips(players, same_hand_weight_list, players_hands)
-
-
-        #Double pair
-        if maks_value == 2:
-            return solve_draw_Two_Pairs(players,same_hand_weight_list,players_hands)
-
-
-        #pair
-        if maks_value == 1:
-            return solve_draw_One_Pair(players, same_hand_weight_list, players_hands)
-
-        # biggest card
-        if maks_value == 0:
-            return solve_draw_biggest_card(players,same_hand_weight_list, players_hands)
+        return solve_draw(players_same_weight, players_hands_same_weight)
 
 
 
 if __name__ == "__main__":
 
     card1 = Card(14, 1)
-    card2 = Card(13, 0)
-    card3 = Card(11, 3)
-    card4 = Card(8, 1)
-    card5 = Card(10, 2)
+    card2 = Card(13, 1)
+    card3 = Card(10, 1)
+    card4 = Card(3, 1)
+    card5 = Card(2, 2)
 
 
-    card6 = Card(6, 1)
-    card7 = Card(2, 2)
-    card8 = Card(6,1)
-    card9 = Card(9,2)
+    card6 = Card(6, 2)
+    card7 = Card(12, 2)
+    card8 = Card(4,3)
+    card9 = Card(5,2)
     player1 = Player(100, 0, "lolo")
     player2 = Player(100, 1, "soso")
 
@@ -366,10 +224,13 @@ if __name__ == "__main__":
     print(evel_hand(players[0].cards + table))
     print(evel_hand(players[1].cards + table))
 
+
     res1 = evel_hand(players[0].cards + table)
     res = point_the_winner(players, table)
     for i in range(len(res)):
         print(res[i])
 
-    #poker(players[1].cards + table)
+
+
+
 
---
+++ testy/point_winner_test.py
@@ -0,0 +1,77 @@
+import unittest
+from classes.card import Card
+from classes.layouts import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_winners(self):
+        table = [Card(7,3), Card(6,1), Card(6,2), Card(3,1), Card(2,1)]
+        player1 = Player(100, 0, "lolo")
+        player2 = Player(100, 1, "soso")
+        player3 = Player(100, 2, "koko")
+        players = [player1, player2, player3]
+
+        player1.add_cards([Card(11,1), Card(7,2)])
+        player2.add_cards([Card(14,1), Card(7,3)])
+
+        self.assertEqual([player2], point_the_winner([player1,player2], table))
+        for p in players: p.remove_cards()
+        #best hand on table, available for everyone
+        table = [Card(14, 1), Card(13, 1), Card(12, 1), Card(11, 1), Card(10, 1)]
+        player1.add_cards([Card(11, 2), Card(7, 2)])
+        player2.add_cards([Card(14, 2), Card(7, 3)])
+        player3.add_cards([Card(11, 3), Card(7, 0)])
+        self.assertEqual([player1, player2, player3], point_the_winner([player1, player2, player3], table))
+        for p in players: p.remove_cards()
+
+        #player3 - flush, everyone got straight
+        table = [Card(14, 3), Card(13, 1), Card(12, 1), Card(11, 1), Card(10, 1)]
+        player1.add_cards([Card(11, 2), Card(7, 2)])
+        player2.add_cards([Card(14, 2), Card(7, 3)])
+        player3.add_cards([Card(11, 3), Card(7, 1)])
+        self.assertEqual([player3], point_the_winner([player1, player2, player3], table))
+        for p in players: p.remove_cards()
+
+        table = [Card(12, 1), Card(4, 1), Card(7, 1), Card(5, 1), Card(8, 2)]
+        player1.add_cards([Card(10, 1), Card(11, 1)])
+        player2.add_cards([Card(2, 1), Card(8, 1)])
+        player3.add_cards([Card(12, 3), Card(12, 2)])
+        self.assertEqual([player1], point_the_winner([player1, player2, player3], table))
+        for p in players: p.remove_cards()
+
+        table = [Card(11, 3), Card(11, 0), Card(9, 0), Card(14, 3), Card(2, 3)]
+        player1.add_cards([Card(7, 0), Card(8, 3)])
+        player2.add_cards([Card(13, 2), Card(4, 1)])
+        player3.add_cards([Card(14, 1), Card(10, 2)])
+        self.assertEqual([player3], point_the_winner([player1, player2, player3], table))
+        for p in players: p.remove_cards()
+
+        table = [Card(14, 3), Card(13, 3), Card(12, 3), Card(11, 3), Card(10, 3)]
+        player1.add_cards([Card(2, 0), Card(3, 0)])
+        player2.add_cards([Card(2, 1), Card(3, 1)])
+        player3.add_cards([Card(2, 2), Card(3, 2)])
+        self.assertEqual([player1, player2, player3], point_the_winner([player1, player2, player3], table))
+        for p in players: p.remove_cards()
+
+        table = [Card(2, 1), Card(2, 2), Card(5, 2), Card(14, 0), Card(10, 3)]
+        player1.add_cards([Card(5, 0), Card(5, 3)])
+        player2.add_cards([Card(14, 1), Card(14, 2)])
+        player3.add_cards([Card(9, 2), Card(10, 2)])
+        self.assertEqual([player2], point_the_winner([player1, player2, player3], table))
+        for p in players: p.remove_cards()
+
+        table = [Card(10, 0), Card(14, 0), Card(13, 2), Card(8, 1), Card(2, 2)]
+        player1.add_cards([Card(13, 3), Card(14, 3)])
+        player2.add_cards([Card(10, 1), Card(10, 2)])
+        player3.add_cards([Card(7, 2), Card(7, 1)])
+        self.assertEqual([player2], point_the_winner([player1, player2, player3], table))
+        for p in players: p.remove_cards()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
