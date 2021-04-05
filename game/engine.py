2e74373544494ecad2f30656534343eabace2f59
Pirgo <pierogmateusz@wp.pl>
2021-04-05 19:48:19 +0200

    some fixes in engine

--- game/engine.py
+++ game/engine.py
@@ -1,20 +1,17 @@
 from classes.table import Table
 from classes.player import Player
 from classes.layouts import *
+from classes.game_info import GameInfo
 import itertools
 import time
 
 #zawsze potrzebny jest stol
-game_room = Table()
-#moze dodac te dane do table jako osobna klase
-time_in_round = 30
-small_blind = 5
-big_blind = small_blind * 2
-biggest_bet = big_blind
+game_info = GameInfo(5, 30)
+game_room = Table(game_info)
 #pierw łaczenie, wymagane 3 graczy aby rozpoczac, wstepne zalozenie, id teraz sa od góry ustawione
 p1 = Player(1000, 0, "lolo")
 p2 = Player(1000, 1, "koko")
-p3 = Player(1000, 2, "soso")
+p3 = Player(100, 2, "soso")
 p4 = Player(1000, 3, "soso")
 
 game_room.add_player(p1)
@@ -22,119 +19,101 @@ game_room.add_player(p2)
 game_room.add_player(p3)
 game_room.add_player(p4)
 
-def check(player):
-    global biggest_bet
-    game_room.add_to_pool(biggest_bet - player.tokens_in_pool, player)
-
-
-def raisee(player):
-    global biggest_bet
-    #narazie na sztywno podbicie jest zawsze o 10
-    game_room.add_to_pool(10, player)
-    biggest_bet += 10
-
-
-def fold(player, players_left):
-    players_left.remove(player)
-    return players_left
-
 def main():
-    global biggest_bet
     #start gry, głowna petla odpowiada za rundy
     for i in range(4):
         print("NEW ROUND")
-        players_left_in_round = game_room.players.copy()
+        game_room.update_players_in_round()
         #tutaj pobieramy small i big blinda od odpowiednich osob, ostatnia osoba w liscie jest dealerem
-        game_room.add_to_pool(small_blind, game_room.players[0])
-        game_room.add_to_pool(big_blind, game_room.players[1])
+        game_room.add_to_pool(game_room.get_small_blind(), game_room.players[0])
+        game_room.add_to_pool(game_room.get_big_blind(), game_room.players[1])
         #na poczatek zawsze rozdajemy karty graczom
         game_room.deal_cards_players()
         #te petle to te rozdania
-        if len(players_left_in_round) > 1:
-            players_number = len(players_left_in_round)
+        if len(game_room.players_in_round) > 1:
+            players_number = len(game_room.players)
             for idx, p in enumerate(itertools.cycle(game_room.players), 1):
-                if p in players_left_in_round and idx != 1 and idx != 2 and len(players_left_in_round) > 1:
+                if p in game_room.players_in_round and idx != 1 and idx != 2 and len(game_room.players_in_round) > 1:
                     print(p)
                     print(p.tokens_in_pool)
                     what_to_do = input()
                     if what_to_do == "fold":
-                        players_left_in_round = fold(p, players_left_in_round)
+                        p.fold(game_room)
                     if what_to_do == "raise":
-                        raisee(p)
+                        p.raisee(game_room, 100)
                     if what_to_do == "check":
-                        check(p)
-                if all(p.tokens_in_pool == biggest_bet for p in players_left_in_round) and idx >= players_number+2:
+                        p.check()
+                    if what_to_do == "call":
+                        p.call(game_room)
+                if all(p.tokens_in_pool == game_room.get_biggest_bet() or p.tokens == 0 for p in game_room.players_in_round) and idx >= players_number+2:
                     break
 
         game_room.deal_flop()
         print("AFTER FLOP (3karty pokazane na stole)")
-        if len(players_left_in_round) > 1:
-            players_number = len(players_left_in_round)
+        print(game_room.players_in_round)
+        if len(game_room.players_in_round) > 1:
+            players_number = len(game_room.players)
             for idx, p in enumerate(itertools.cycle(game_room.players), 1):
-                if p in players_left_in_round and len(players_left_in_round) > 1:
+                if p in game_room.players_in_round and len(game_room.players_in_round) > 1:
                     print(p)
                     print(p.tokens_in_pool)
                     what_to_do = input()
                     if what_to_do == "fold":
-                        players_left_in_round = fold(p, players_left_in_round)
+                        p.fold(game_room)
                     if what_to_do == "raise":
-                        raisee(p)
+                        p.raisee(game_room, 100)
                     if what_to_do == "check":
-                        check(p)
-                if all(p.tokens_in_pool == biggest_bet for p in players_left_in_round) and idx >= players_number:
+                        p.check()
+                    if what_to_do == "call":
+                        p.call(game_room)
+                if all(p.tokens_in_pool == game_room.get_biggest_bet() or p.tokens == 0 for p in game_room.players_in_round) and idx >= players_number:
                     break
         game_room.deal_turn_river()
         print("AFTER TURN (kolejna karta na stole)")
-        print(players_left_in_round)
-        if len(players_left_in_round) > 1:
-            players_number = len(players_left_in_round)
+        if len(game_room.players_in_round) > 1:
+            players_number = len(game_room.players)
             for idx, p in enumerate(itertools.cycle(game_room.players), 1):
-                if p in players_left_in_round and len(players_left_in_round) > 1:
+                if p in game_room.players_in_round and len(game_room.players_in_round) > 1:
                     print(p)
                     print(p.tokens_in_pool)
                     what_to_do = input()
                     if what_to_do == "fold":
-                        players_left_in_round = fold(p, players_left_in_round)
+                        p.fold(game_room)
                     if what_to_do == "raise":
-                        raisee(p)
+                        p.raisee(game_room, 100)
                     if what_to_do == "check":
-                        check(p)
-                if all(p.tokens_in_pool == biggest_bet for p in players_left_in_round) and idx >= players_number:
+                        p.check()
+                    if what_to_do == "call":
+                        p.call(game_room)
+                if all(p.tokens_in_pool == game_room.get_biggest_bet() or p.tokens == 0 for p in
+                       game_room.players_in_round) and idx >= players_number:
                     break
         game_room.deal_turn_river()
         print("AFTER RIVER (kolejna karta na stole)")
-        if len(players_left_in_round) > 1:
-            players_number = len(players_left_in_round)
+        if len(game_room.players_in_round) > 1:
+            players_number = len(game_room.players)
             for idx, p in enumerate(itertools.cycle(game_room.players), 1):
-                if p in players_left_in_round and len(players_left_in_round) > 1:
+                if p in game_room.players_in_round and len(game_room.players_in_round) > 1:
                     print(p)
                     print(p.tokens_in_pool)
                     what_to_do = input()
                     if what_to_do == "fold":
-                        players_left_in_round = fold(p, players_left_in_round)
+                        p.fold(game_room)
                     if what_to_do == "raise":
-                        raisee(p)
+                        p.raisee(game_room, 100)
                     if what_to_do == "check":
-                        check(p)
-                if all(p.tokens_in_pool == biggest_bet for p in players_left_in_round) and idx >= players_number:
+                        p.check()
+                    if what_to_do == "call":
+                        p.call(game_room)
+                if all(p.tokens_in_pool == game_room.get_biggest_bet() or p.tokens == 0 for p in
+                       game_room.players_in_round) and idx >= players_number:
                     break
-        for p in players_left_in_round:
-            print(p)
-
-        winners = point_the_winner(players_left_in_round, game_room.tableCards)
-        game_room.give_prize_tw(winners)
-        print("--------------")
-        print(winners)
+        print(game_room.tableCards)
+        game_room.reveal_winners()
+        print(game_room.players)
 
 
-        #po ustaleniu zwyciezcy wszyscy gracze zwracaja karty, ze stolu tez znikaja
-        #ta funkcja usuwa karty i zetony z puli kazdego gracza
-        game_room.remove_cards_players()
-        game_room.remove_cards_table()
-        game_room.deck.push_cards()
-        biggest_bet = big_blind
-        #tu kod na usuniecie z gry
-        game_room.move_dealer()
+        game_room.new_round()
 
 
 
